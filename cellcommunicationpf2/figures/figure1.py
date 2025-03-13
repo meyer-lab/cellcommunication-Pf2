54ec4fee03ec5cb94169970235234e3a99546d5a
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2025-03-13 14:57:07 -0700

    Add function that converts CCC anndata to tensor

--- cellcommunicationpf2/figures/figure1.py
+++ cellcommunicationpf2/figures/figure1.py
@@ -7,6 +7,10 @@ from ..import_data import (
     import_balf_covid,
     import_ligand_receptor_pairs,
     anndata_lrp_overlap,
+    add_cond_idxs,
+    anndata_to_tensor
+    
+    
 )
 from ..ccc import calc_communication_score
 
@@ -24,5 +28,14 @@ def makeFigure():
     df_lrp = df_lrp.iloc[:20, :]
 
     Xccc = calc_communication_score(X, df_lrp, communication_score="expression_product")
+    
+    Xccc = add_cond_idxs(Xccc, "sample")
+    print(Xccc)
+    X_list = anndata_to_tensor(Xccc)
+
+    assert len(X_list) == len(Xccc.obs["condition_unique_idxs"].unique())
+    assert len(X_list[0].shape) == 3
+    assert X_list[0].shape[0] == X_list[0].shape[1]
+    assert X_list[0].shape[2] == X_list[1].shape[2] 
 
     return f
