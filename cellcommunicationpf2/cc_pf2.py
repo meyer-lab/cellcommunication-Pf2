2a122f8cc1917f77ecfbc16b3d26514f0eaabb7f
Nathaniel Thomas <catchnate@gmail.com>
2025-07-15 09:58:58 -0700

    Rename ambiguous variable

--- cellcommunicationpf2/cc_pf2.py
+++ cellcommunicationpf2/cc_pf2.py
@@ -203,30 +203,30 @@ def standardize_cc_pf2(
     weights, factors = cp_flip_sign(cp_normalize((weights, factors)), mode=1)
 
     for i in [1, 2]:  # For sender and receiver factors
-        M = factors[i]
-        rows, cols = M.shape
+        curr_factor = factors[i]
+        rows, cols = curr_factor.shape
         k = min(rows, cols)
         # Truncate to square matrix
         if rows != cols:
-            M = M[:k, :k]
+            curr_factor = curr_factor[:k, :k]
 
         # Handle the ordering and sign flipping based on whether rows or columns are larger
         if rows > cols:
             # Order eigen-cells to maximize the diagonal of B/C
-            _, col_ind = linear_sum_assignment(np.abs(M), maximize=True)
+            _, col_ind = linear_sum_assignment(np.abs(curr_factor), maximize=True)
             factors[i] = factors[i][:, col_ind]
             projections = [p[:, col_ind] for p in projections]
         else:
             # Order eigen-cells to maximize the diagonal of B/C
-            _, row_ind = linear_sum_assignment(np.abs(M.T), maximize=True)
+            _, row_ind = linear_sum_assignment(np.abs(curr_factor.T), maximize=True)
             factors[i] = factors[i][row_ind, :]
             projections = [p[row_ind, :] for p in projections]
 
         # Flip the sign based on B/C
         signn = np.sign(np.diag(factors[i][:k, :k]))
         # If the number of columns is larger, extend the sign vector with ones
-        if len(signn) < M.shape[1]:
-            signn = np.concatenate([signn, np.ones(M.shape[1] - len(signn))])
+        if len(signn) < curr_factor.shape[1]:
+            signn = np.concatenate([signn, np.ones(curr_factor.shape[1] - len(signn))])
         # Apply the sign to both factors and projections
         factors[i] *= signn[:, np.newaxis]
         projections = [p * signn for p in projections]
