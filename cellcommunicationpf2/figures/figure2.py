694fa35e30dd2541c0a6102709ed3cb8060a3501
Nathaniel Thomas <catchnate@gmail.com>
2025-07-17 12:47:58 -0700

    Fix call

--- cellcommunicationpf2/figures/figure2.py
+++ cellcommunicationpf2/figures/figure2.py
@@ -66,7 +66,7 @@ def run_cc_pf2_analysis(
     cp_results, projections = results
     cp_weights, factors = cp_results
 
-    _, factors, _ = standardize_cc_pf2(factors, projections, weights=cp_weights)
+    _, factors, _ = standardize_cc_pf2(factors, weights=cp_weights)
 
     adata.uns["Pf2_A"] = factors[0]
     adata.uns["Pf2_B"] = factors[1]
