98edd00b699c30d97ddf25b4c63a34fecaea02be
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2025-08-27 04:21:09 -0700

    Plot both frac and mean

--- cellcommunicationpf2/figures/figureA2.py
+++ cellcommunicationpf2/figures/figureA2.py
@@ -19,16 +19,19 @@ from ..cc_pf2 import (
     calc_communication_score_pseudobulk,
     pseudobulk_nncp_decomposition,
 )
-# from .commonFuncs.plotFactors import (
-#     plot_condition_factors,
-#     plot_eigenstate_factors,
-#     plot_lr_factors,
-#     rotate_yaxis
-# )
+from .commonFuncs.plotFactors import (
+    plot_condition_factors,
+    plot_eigenstate_factors,
+    plot_lr_factors
+)
+
+from .commonFuncs.plotGeneral import (
+    rotate_yaxis
+)
 
 
 def makeFigure():
-    ax, f = getSetup((18, 18), (2, 2))
+    ax, f = getSetup((24, 12), (2, 4))
     subplotLabel(ax)
 
     # Import and prepare data
@@ -48,17 +51,58 @@ def makeFigure():
     ).set_index(condition_column)[group_col]
 
     groupby = "celltype"
-    appended_pseudobulk  = pseudobulk_X(X_filtered, condition_name=condition_column, groupby=groupby, type="mean")
-    interaction_tensor, filtered_lr_pairs = calc_communication_score_pseudobulk(appended_pseudobulk, lr_pairs=lr_pairs)
+    groupby_names = X_filtered.obs[groupby].unique()
+    types = ["mean", "fraction"]
+    for i, t in enumerate(types):
+        appended_pseudobulk = pseudobulk_X(X_filtered, condition_name=condition_column, groupby=groupby, type=t)
+        interaction_tensor, filtered_lr_pairs = calc_communication_score_pseudobulk(appended_pseudobulk, lr_pairs=lr_pairs)
+
+        _, cpd_factors, _ = pseudobulk_nncp_decomposition(interaction_tensor, cp_rank=10, n_iter_max=10000, tol=1e-9)
+        # Confirm cpd factors are only positive
+        for factor in cpd_factors:
+          assert np.all(factor >= 0), "CPD factors contain negative values"
+            
+        X_filtered.uns["Pf2_A"] = cpd_factors[0]  # Condition factor
+        X_filtered.uns["Pf2_B"] = cpd_factors[1]  # Sender cell types factor
+        X_filtered.uns["Pf2_C"] = cpd_factors[2]  # Receiver cell types factor
+        X_filtered.uns["Pf2_D"] = cpd_factors[3]  # LR pairs factor
+        X_filtered.uns["Pf2_lr_pairs"] = filtered_lr_pairs  # LR pairs
+
+        plot_condition_factors(
+            data=X_filtered,
+            ax=ax[2*i],
+            cond=condition_column,
+            cond_group_labels=sample_to_group,
+            group_cond=True,
+        )
+  
+        plot_eigenstate_factors(
+            data=X_filtered,
+            ax=ax[2*i+1],
+            factor_type="Pf2_B",
+        )
+        ax[2*i+1].set_yticklabels(groupby_names)
+
+        plot_eigenstate_factors(
+            data=X_filtered,
+            ax=ax[2*i+2],
+            factor_type="Pf2_C",
+        )
+        ax[2*i+2].set_yticklabels(groupby_names)
 
 
-    print(interaction_tensor.shape)
-    print(filtered_lr_pairs)
-
-    _, cpd_factors, _ = pseudobulk_nncp_decomposition(interaction_tensor, cp_rank=10, n_iter_max=10000, tol=1e-9)
-    # Confirm cpd factors are only positive
-    for factor in cpd_factors:
-        assert np.all(factor >= 0), "CPD factors contain negative values"
+        plot_lr_factors(
+            data=X_filtered,
+            ax=ax[2*i+3],
+            weight=0.07,
+        )
+        
+        ax[2*i].set_title("Conditions Factor")
+        ax[2*i+1].set_title("Sender Cell Type Factor")
+        ax[2*i+2].set_title("Receiver Cell Type Factor")
+        ax[2*i+3].set_title("Ligand-Receptor Factor")
+        rotate_yaxis(ax[2*i+1], rotation=0)
+        rotate_yaxis(ax[2*i+2], rotation=0)
 
     return f
 
