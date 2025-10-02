58c3627dace4c027f73dd27a668f0e81c68564c8
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2025-10-02 15:08:29 -0700

    Add all supplemental figures

---
+++ cellcommunicationpf2/figures/figureS6.py
@@ -0,0 +1,23 @@
+"""
+Figure S6: CCC-RISE on BALF alad data. Showing ligand-receptor factors.
+"""
+
+from .common import (
+    subplotLabel,
+    getSetup,
+)
+import anndata
+from .commonFuncs.plotFactors import plot_lr_factors_partial
+
+
+def makeFigure():
+    ax, f = getSetup((12, 12), (6, 6))
+    subplotLabel(ax)
+
+    X = anndata.read_h5ad("/opt/andrew/ccc/bal_alad.h5ad")
+
+    for i in range(X.uns["A"].shape[1]):
+        plot_lr_factors_partial(X, i + 1, ax[2 * i], geneAmount=10, top=True)
+        plot_lr_factors_partial(X, i + 1, ax[2 * i + 1], geneAmount=10, top=False)
+
+    return f
