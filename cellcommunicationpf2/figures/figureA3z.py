1ab9ae960063d00431c9545c60b6dc8751b76409
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2026-01-27 07:58:18 -0800

    Genes associated with cell population

---
+++ cellcommunicationpf2/figures/figureA3z.py
@@ -0,0 +1,59 @@
+"""
+Figure A3z: XXX
+"""
+
+import anndata
+from .common import (
+    subplotLabel,
+    getSetup,
+)
+import numpy as np
+import seaborn as sns
+from ..utils import (
+    add_obs_cmp_label,
+    add_obs_cmp_unique_one,
+)
+import scanpy as sc
+
+
+def makeFigure():
+    ax, f = getSetup((6, 6), (2, 2))
+    subplotLabel(ax)
+
+    X = anndata.read_h5ad("/opt/andrew/ccc/bal_covid19.h5ad")
+    ccc_rise_cmp = 5
+    pos_perc = [True, False]
+    axs = 0
+    
+    for i in pos_perc:
+        X_epi_sender = X[X.obs["celltype"] == "Epithelial"]
+        X_epi_sender = add_obs_cmp_label(X_epi_sender, cmp=ccc_rise_cmp, pos=i, top_perc=10, type="sender")
+        X_epi_sender = add_obs_cmp_unique_one(X_epi_sender, cmp=ccc_rise_cmp)
+  
+        # Make ["Label"] categorical codes (Cmp5 is 0 for Top Perc, 1 for Others)
+        cmp_label = X_epi_sender.obs["Label"].astype("category").cat.codes
+
+        method_test = ["t-test", "wilcoxon"]
+        
+        for method in method_test:
+            sc.tl.rank_genes_groups(X_epi_sender, "Label", method=method)
+            results = X_epi_sender.uns["rank_genes_groups"]
+            top_n = 20
+            top_idx = np.argsort(results['scores']['Cmp5'])[-top_n:][::-1]
+            
+            top_genes = results['names']['Cmp5'][top_idx]
+            top_scores = results['scores']['Cmp5'][top_idx]
+            
+            sns.barplot(
+                x=top_scores,
+                y=top_genes,
+                ax=ax[axs],
+            )
+            ax[axs].set_xlabel(method)
+            ax[axs].set_ylabel("Gene")
+            axs += 1
+        
+
+
+    return f
+
